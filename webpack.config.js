fcdc6aa326a9e3b77ee69c3e142d5d8e466211fb
Vadim Brikulya <im.master99@gmail.com>
2021-04-16 21:24:21 +0300

    update setting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
-module.export ={
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
     module: {
-        
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+           }
+       ]    
     },
-    plugin: []
-}
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+    })]
+};
